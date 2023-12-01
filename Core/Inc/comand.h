dc8ea881ec8e7023ad0c367472430e5328a989b9
Nicolasbern <jubernalb@unal.edu.co>
2023-12-01 18:48:13 -0500

    feat(update): data update.

---
+++ Core/Inc/comand.h
@@ -0,0 +1,13 @@
+/*
+ * comand.h
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: Nico
+ */
+
+#ifndef INC_COMAND_H_
+#define INC_COMAND_H_
+
+#include <stdint.h>
+
+#endif /* INC_COMAND_H_ */
---
+++ Core/Src/comand.c
@@ -0,0 +1,8 @@
+/*
+ * comand.c
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: Nico
+ */
+
+
